b519c58ea9bf4ee576468765d5edd0afc88f03a4
ururcarla <tisato141@gmail.com>
2025-11-29 14:05:20 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+import os
 from setuptools import setup, Extension
 import numpy as np
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+common_dir = os.path.join(this_dir, 'common')
+pycoco_dir = os.path.join(this_dir, 'pycocotools')
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
@@ -7,8 +12,11 @@ import numpy as np
 ext_modules = [
     Extension(
         'pycocotools._mask',
-        sources=['common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), 'common'],
+        sources=[
+            os.path.join(common_dir, 'maskApi.c'),
+            os.path.join(pycoco_dir, '_mask.pyx'),
+        ],
+        include_dirs = [np.get_include(), common_dir],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
